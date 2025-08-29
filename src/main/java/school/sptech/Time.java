6d0e5a168c6611e2b10928f8080e513ba52c764a
Nicolas Barbosa <nicolas.pereira009@sptech.school>
2025-08-29 14:13:25 -0300

    exercicio POO

--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,59 @@
 package school.sptech;
 
 public class Time {
+    String nome;
+    Integer vitorias=0;
+    Integer empates=0;
+    Integer derrotas=0;
+
+    public Time() {
+    }
+
+    public void registrarVitoria(){
+        vitorias++;
+    }
+
+    public void registrarEmpate(){
+        empates++;
+    }
+
+    public void registrarDerrota(){
+        derrotas++;
+    }
+    public Integer getPontos(){
+        return (vitorias*3)+empates;
+    }
+
+    public Integer getTotalPartidas(){
+        return vitorias+empates+derrotas;
+    }
+
+    public Integer getAproveitamento() {
+        int total = getTotalPartidas();
+        double aproveitamento = ((double) vitorias / total) * 100;
+        return (int) aproveitamento;
+    }
+
+
+    public void compararAproveitamento(Time adversario){
+    Integer timeAtual=getAproveitamento();
+    Integer timeInimigo= adversario.getAproveitamento();
+    String melhor;
+    if(timeAtual<timeInimigo){
+        melhor="o melhor time foi o: "+adversario.nome;
+    }else if(timeAtual>timeInimigo){
+            melhor = "o melhor time foi o: "+ nome;
+        }else{
+        melhor="Os dois times tiveram um aproveitamento igual de: "+ getAproveitamento();
+        }
+        System.out.println(melhor);
+    }
+
+
+
+
+
+
+
 
 }
